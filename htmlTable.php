dc7cdd534ac538988129e353c5bb04454a9a78db
Michael Ajala <mda9@njit.edu>
2017-10-21 22:14:59 -0400

    commit for the htmlTable

---
+++ htmlTable.php
@@ -0,0 +1,57 @@
+<?php
+
+
+
+
+class htmlTable extends page {
+
+    public function get(){
+
+        $location2='uploads/';
+        $name1=$_GET['file'];
+        echo $name1;
+        $file_name=fopen($location2.$name1, "r");
+        $row = 1;
+        if (($handle = fopen($location2.$name1, "r")) !== FALSE) {
+        echo '<table border="1">';
+    
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        if ($row == 1) {
+            echo '<thead><tr>';
+        }else{
+            echo '<tr>';
+        }
+        
+        for ($c=0; $c < $num; $c++) {
+            //echo $data[$c] . "<br />\n";
+            if(empty($data[$c])) {
+               $value = "&nbsp;";
+            }else{
+               $value = $data[$c];
+            }
+            if ($row == 1) {
+                echo '<th>'.$value.'</th>';
+            }else{
+                echo '<td>'.$value.'</td>';
+            }
+        }
+        if ($row == 1) {
+            echo '</tr></thead><tbody>';
+        }else{
+            echo '</tr>';
+        }
+        $row++;
+        }
+        echo '</tbody></table>';
+        fclose($handle);
+}       
+    }
+    
+}
+
+
+
+
+
+?>
